6ba5423cc315be41d69a59bf54f0761a19be851e
mahmody <42693632+mahmody@users.noreply.github.com>
2019-02-18 22:15:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3523,10 +3523,6 @@ client.on("message", (message) => {
     }
 });
 	
-client.on('guildMemberAdd', member => {
-    member.createDM().then(function (channel) {
-return channel.send("**`افضل موقع بيديك حسابات مجانا لكل الالعاب ال ممكن تتخيلها` \n [https://to.free-gg.com/VYOeHU0]**")
-	    
-    });
+
 	
 client.login(process.env.BOT_TOKEN)
